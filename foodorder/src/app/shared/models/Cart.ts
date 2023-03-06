2372b191d5cbe81a7a3a14c9f026d6943d72e36e
Bhushan Borde <121348007+bhushanborde2000github@users.noreply.github.com>
2023-03-06 16:10:29 +0530

    Add files via upload

---
+++ foodorder/src/app/shared/models/Cart.ts
@@ -0,0 +1,17 @@
+import { CartItem } from "./CartItem";
+
+export class Cart {
+    items:CartItem [] = [];
+
+    get totalPrice():number{
+        let totalPrice = 0;
+        this.items.forEach(item => {
+            totalPrice += item.price
+
+        });
+
+        return totalPrice;
+    }
+}
+
+export { CartItem };
